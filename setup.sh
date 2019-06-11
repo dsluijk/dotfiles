e9c869da86708d336ef4a38189bd2af9c22d211f
Dany Sluijk <dany@atlasdev.nl>
2019-06-11 21:19:34 +0200

    feat: add pgp key and remove unneeded change shell

--- setup.sh
+++ setup.sh
@@ -2,13 +2,17 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-yay -Sy --noconfirm --needed fish neovim htop
-
-chsh -s /usr/bin/fish
+yay -Sy --noconfirm --needed fish neovim htop wget
 
 if [ $SHELL != "/usr/bin/fish" ]
 then
     chsh -s /usr/bin/fish
 fi
 
+wget https://keybase.io/atlasdev/pgp_keys.asc -O /tmp/pgp.asc
+gpg --import /tmp/pgp.asc
+
 ln -sf ${BASEDIR}/gitconfig ~/.gitconfig
+
+echo "All done!"
+echo "Please check and trust the pgp key added to the chain so it can be checked."
