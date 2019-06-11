3a3015fcd2f3f1c2135a54f9418c46e1e4bcfd79
Dany Sluijk <dany@atlasdev.nl>
2019-06-11 18:25:29 +0200

    feat: add shell change and neovim

--- setup.sh
+++ setup.sh
@@ -2,4 +2,13 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+yay -Sy --noconfirm --needed fish neovim
+
+chsh -s /usr/bin/fish
+
+if [ $SHELL != "/usr/bin/fish" ]
+then
+    chsh -s /usr/bin/fish
+fi
+
 ln -sf ${BASEDIR}/gitconfig ~/.gitconfig
