ab013abc9e8917810c0ea2910788dadf21fcd87d
Dany Sluijk <dany@atlasdev.nl>
2019-06-26 23:25:22 +0200

    refactor: change a few configuration files

--- setup.sh
+++ setup.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
+if [ "$EUID" -eq 0 ]
+	then echo "Please don't run as root"
+	exit
+fi
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WORK=`pwd`
 
-yay -Sy --noconfirm --needed fish neovim htop wget ccid bspwm sxhkd urxvt polybar xorg-xrandr
+sudo pacman -S --needed git
 
-if [ $SHELL != "/usr/bin/fish" ]
+if ! pacman -Qq | grep -qw yay; then
+	git clone https://aur.archlinux.org/yay.git /tmp/yay
+	cd /tmp/yay
+	makepkg -si
+	cd ${WORK} 
+fi
+
+yay -Sy --noconfirm --needed zsh neovim htop wget ccid bspwm sxhkd polybar xorg-xrandr btops-git neofetch rofi ranger
+
+if ! pacman -Qq | grep -qw st; then
+	git clone https://git.suckless.org/st /tmp/st
+	cd /tmp/st
+	ls
+	cd ${WORK}
+fi
+
+if [ $SHELL != "/usr/bin/zsh" ]
 then
-    chsh -s /usr/bin/fish
+	chsh -s /usr/bin/zsh
 fi
 
 sudo systemctl enable pcscd
@@ -21,14 +43,18 @@ mkdir -p ~/.ssh
 
 ln -sf ${BASEDIR}/gitconfig ~/.gitconfig
 ln -sf ${BASEDIR}/ssh/authorized_keys ~/.ssh/authorized_keys
+ln -sf ${BASEDIR}/ssh/config ~/.ssh/config
 ln -sf ${BASEDIR}/xinitrc ~/.xinitrc
 ln -sf ${BASEDIR}/config/bspwm ~/.config
 ln -sf ${BASEDIR}/config/sxhkd ~/.config
 ln -sf ${BASEDIR}/config/polybar ~/.config
-ln -sf ${BASEDIR}/config/fish ~/.config
+ln -sf ${BASEDIR}/config/zsh/zshrc ~/.zshrc
+ln -sf ${BASEDIR}/config/zsh/zlogin ~/.zlogin
+ln -sf ${BASEDIR}/config/rofi ~/.config
 
-set -U fish_greeting
 chmod +x ~/.config/polybar/launch.sh
+
+neofetch
 
 echo "All done!"
 echo "Please check and trust the pgp key added to the chain so it can be checked."
