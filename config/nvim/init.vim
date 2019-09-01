af33320e968294ecad1afc72a3a0de62cec45704
Dany Sluijk <dany@atlasdev.nl>
2019-09-01 17:47:18 +0200

    feat: more configuration

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -1,15 +1,22 @@
 call plug#begin('~/.local/share/nvim/plugged')
 
 Plug 'morhetz/gruvbox'
+Plug 'dylanaraps/wal.vim'
 Plug 'vim-airline/vim-airline'
 Plug 'scrooloose/nerdtree'
-Plug 'rust-lang/rust'
+Plug 'rust-lang/rust.vim'
+Plug 'editorconfig/editorconfig-vim'
+Plug 'leafgarland/typescript-vim'
 Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'sbdchd/neoformat'
+Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
 
 call plug#end()
 
-colorscheme gruvbox
+" colorscheme gruvbox
+colorscheme wal
 
+set number
 set tabstop=4
 set softtabstop=4
 set shiftwidth=4
@@ -19,11 +26,16 @@ filetype plugin indent on
 syntax on
 set spell
 
+let g:rustfmt_autosave = 1
+
+autocmd BufWritePre,InsertLeave * undojoin | Neoformat
+
 let g:airline#extensions#ale#enabled = 1
 let g:airline#extensions#tabline#enabled = 1
 let g:airline_section=''
 let g:airline_detect_paste=1
 let g:airline_powerline_fonts = 1
+
 if !exists('g:airline_symbols')
 	let g:airline_symbols = {}
 endif
--- setup.sh
+++ setup.sh
@@ -17,14 +17,7 @@ if ! pacman -Qq | grep -qw yay; then
 	cd ${WORK} 
 fi
 
-yay -Sy --noconfirm --needed zsh zsh-syntax-highlighting neovim htop wget ccid bspwm sxhkd polybar xorg-xrandr btops-git neofetch rofi ranger
-
-if ! pacman -Qq | grep -qw st; then
-	git clone https://git.suckless.org/st /tmp/st
-	cd /tmp/st
-	ls
-	cd ${WORK}
-fi
+yay -Sy --noconfirm --needed zsh zsh-syntax-highlighting alacritty neovim htop wget ccid bspwm sxhkd polybar xorg-xrandr btops-git neofetch rofi ranger npm nodejs rustup go go-tools python-pywal maim xclip
 
 if [ $SHELL != "/usr/bin/zsh" ]
 then
@@ -45,8 +38,11 @@ ln -sf ${BASEDIR}/gitconfig ~/.gitconfig
 ln -sf ${BASEDIR}/ssh/authorized_keys ~/.ssh/authorized_keys
 ln -sf ${BASEDIR}/ssh/config ~/.ssh/config
 ln -sf ${BASEDIR}/xinitrc ~/.xinitrc
+ln -sf ${BASEDIR}/wallpapers ~/.wallpapers
 ln -sf ${BASEDIR}/config/bspwm ~/.config
+ln -sf ${BASEDIR}/config/btops ~/.config
 ln -sf ${BASEDIR}/config/sxhkd ~/.config
+ln -sf ${BASEDIR}/config/alacritty ~/.config
 ln -sf ${BASEDIR}/config/polybar ~/.config
 ln -sf ${BASEDIR}/config/zsh/zshrc ~/.zshrc
 ln -sf ${BASEDIR}/config/zsh/zprofile ~/.zprofile
@@ -56,6 +52,14 @@ ln -sf ${BASEDIR}/config/rofi ~/.config
 chmod +x ~/.config/polybar/launch.sh
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+rustup toolchain install stable
+rustup default stable
+
+mkdir -p ~/proj/{src,bin,pkg}
+mkdir ~/.go
+
+sudo npm install --global prettier
 
 neofetch
 
